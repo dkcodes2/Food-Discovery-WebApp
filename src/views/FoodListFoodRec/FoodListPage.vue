f283e4a2cfa138b64412840057933375ed0a33a8
aqfw <116161343+aqfw@users.noreply.github.com>
2023-10-31 16:15:17 +0800

    foodrecpage

---
+++ src/views/FoodListFoodRec/FoodListPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="food-list-page">
+      <h1>Your Food Lists sorted by Tags</h1>
+      <Food :users="followings" />
+    </div>
+</template>
---
+++ src/views/FoodListFoodRec/FoodRecPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="food-rec-page">
+      <h1>Your List of Past Eats</h1>
+      <FoodRecs :recs="pastposts" />
+    </div>
+</template>
+
+<script> 
+
+    import { ref, onMounted } from 'vue';
+    import { db } from '@/firebaseConfig.js'; 
+    import PostsContainerAQ from "@\components\food-list-enabling-components\PostsContainerAQ.vue";
+    
+    export default {
+        name: "FoodRecPage",
+
+        components: {
+            //what do I pass to the FoodRecs item with pastposts? I will pass FoodRecs
+            FoodRecs
+        }, 
+        props: {
+
+        }
+
+        setup() {
+        const pastposts = ref([]);
+            //all the past posts must be given to "pastposts"
+            //each element of pastposts should be the "Caption" of the past post,
+            //and the URL of the past post
+        onMounted(async () => {
+
+            const querySnapshot = await db.collection('pastposts').get();
+            pastposts.value = querySnapshot.docs.map(doc => doc.data ())
+        
+            //here I will calling the array of past-posts to store past post data into an array of past-posts.
+            //I should make it call only the Caption and the URL in which it is stored.
+        });
+        
+        return pastposts;
+        
+        }
+    }
+</script>
+
+    
