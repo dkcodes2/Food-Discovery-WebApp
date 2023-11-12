6c3ba006f54435f69334127330f27383a5fd6e5a
Jaron Tan <e0773479@u.nus.edu>
2023-11-12 18:46:32 +0800

    CSS Done

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -55,8 +55,35 @@ export default {
 </script>
 
 <style scoped>
-h2 {
-    text-align: left;
+.navbar-container {
+    max-width: 100%; /* Full width to center navbar */
+    display: flex;
+    justify-content: center; /* Centers navbar horizontally */
+    padding: 10px 0; /* Add padding if needed */
+    margin: 0 40px;
+}
+
+.posts-container {
+    max-width: 100%; /* Take full width of the parent */
+    display: flex;
+    flex-direction: column;
+    align-self: stretch; /* Stretch to match the parent's width */
+    overflow: hidden;
+    position: relative;
+    height: auto; /* This will maintain the aspect ratio of the image */
+    padding: 11px 0; /* Padding top and bottom, 0 left and right */
+    margin-top: 10px;
+    margin: 0 40px;
+}
+
+.cuisine-container{
+    margin: 0 40px;
+    margin-bottom: 20px
+}
+
+h1 {
+    margin-top:0 40px;
+    margin: 0 40px;
 }
 
 h5 {
